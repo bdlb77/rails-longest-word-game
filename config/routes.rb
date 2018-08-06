74b8bd04fc465e593015ec74245b1dca0bb079f6
Bryan Leighton <bryan.leigh77@gmail.com>
2018-08-06 17:59:49 +0200

    longest word

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   get 'new', to: 'games#new'
   get 'score', to: 'games#score'
 
-  post 'games', to: 'games#score'
+  get 'games', to: 'games#score'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
